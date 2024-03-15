dea4d52d1bf2bd7bc025f31612d61fd1bc62d118
jet <artturi@juvonen.eu>
2024-03-15 13:51:22 +0200

    add Sugiyama layout and pretty formatting for winc2petri_pnml.py

--- winc2petri_pnml.py
+++ winc2petri_pnml.py
@@ -1,48 +1,63 @@
 import sys
 import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
+import networkx as nx
+from networkx.drawing.nx_agraph import write_dot, graphviz_layout
 
 def parse_matrix(matrix_str):
     matrix = eval(matrix_str)
     return matrix
 
 def generate_petri_pnml(matrix):
+    G = nx.DiGraph()
     num_places = len(matrix)
     num_transitions = len(matrix[0])
     places = [f"v_{i+1}" for i in range(num_places)]
     transitions = [f"T_{j+1}" for j in range(num_transitions)]
-    arcs = []
+
     for i in range(num_places):
         for j in range(num_transitions):
             if matrix[i][j] != 0:
                 if matrix[i][j] > 0:
-                    arcs.append((transitions[j], places[i]))
+                    G.add_edge(transitions[j], places[i])
                 else:
-                    arcs.append((places[i], transitions[j]))
-    
+                    G.add_edge(places[i], transitions[j])
+
+    # Use Graphviz for Sugiyama layout
+    pos = graphviz_layout(G, prog="dot")
+
     # Creating PNML structure
     pnml = ET.Element("pnml")
     net = ET.SubElement(pnml, "net", {"id": "information_warfare", "type": "http://www.pnml.org/version-2009/grammar/ptnet"})
-    
+
     # Adding places
     for place_id in places:
+        x, y = pos[place_id]
         place = ET.SubElement(net, "place", {"id": place_id})
-        if any(place_id == arc[0] for arc in arcs):
+        graphics = ET.SubElement(place, "graphics")
+        position = ET.SubElement(graphics, "position", {"x": str(x), "y": str(y)})
+        if any(place_id == arc[0] for arc in G.edges()):
             initial_marking = ET.SubElement(place, "initialMarking")
             text = ET.SubElement(initial_marking, "text")
             text.text = "1"
-    
+
     # Adding transitions
     for transition_id in transitions:
-        ET.SubElement(net, "transition", {"id": transition_id})
-    
+        x, y = pos[transition_id]
+        transition = ET.SubElement(net, "transition", {"id": transition_id})
+        graphics = ET.SubElement(transition, "graphics")
+        position = ET.SubElement(graphics, "position", {"x": str(x), "y": str(y)})
+
     # Adding arcs
-    for arc in arcs:
+    for arc in G.edges():
         source, target = arc
         ET.SubElement(net, "arc", {"id": f"arc_{source}_to_{target}", "source": source, "target": target})
-    
+
     # Generating PNML file
     tree = ET.ElementTree(pnml)
-    tree.write("output_petri.pnml", encoding="utf-8", xml_declaration=True)
+    xml_str = minidom.parseString(ET.tostring(pnml)).toprettyxml(indent="    ")
+    with open("output_petri.pnml", "w") as f:
+        f.write(xml_str)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
